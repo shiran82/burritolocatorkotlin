45e77698bb9c7da17abfa56edc7ce3e842f81c8f
shiranpeer <shiran.peer82@gmail.com>
2018-07-31 12:51:02 +0200

    made retrofit singleton

--- app/src/main/java/com/example/shiranpeer/burritolocator/service/APIClient.java
+++ app/src/main/java/com/example/shiranpeer/burritolocator/service/APIClient.java
@@ -8,23 +8,26 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static okhttp3.internal.Internal.instance;
+
 public class APIClient {
 
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
 
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        if (retrofit == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
 
-        retrofit = new Retrofit.Builder()
-                .baseUrl("https://maps.googleapis.com/maps/api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .client(client)
-                .build();
-
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("https://maps.googleapis.com/maps/api/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(client)
+                    .build();
+        }
         return retrofit;
     }
 }
